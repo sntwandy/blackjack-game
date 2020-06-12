2dd8c141370f872bec73154ca6d7d2f826f13c5d
sntwandy <sntwandy@gmail.com>
2020-06-12 09:13:02 -0400

    Manipulating the DOM cards

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,9 +5,17 @@
  *  2S = Two of Spades
  */
 
+// HTML refences
+const btnRequestCard = document.querySelector('#btnRequestCard');
+const pointsContainer = document.querySelectorAll('small');
+const playerCards = document.querySelector('#player-cards');
+const computerCards = document.querySelector('#computer-cards');
+
+// variables
 let deck = [];
 const cardTypes = ['C', 'D', 'H', 'S'];
 const cardTypesSpecials = ['A', 'J', 'Q', 'K'];
+let playerPoints = 0, computerPoints = 0;
 
 // Create a new deck
 const createDeck = () => {
@@ -25,12 +33,12 @@ const createDeck = () => {
 
     // console.log(deck);
     deck = _.shuffle(deck);
-    console.log(deck);
+    // console.log(deck);
 
     return deck;
 };
 
-createDeck();
+// createDeck();
 
 
 // Pick a card
@@ -42,8 +50,8 @@ const pickCard = () => {
     }
 
     const card = deck.pop();
-    console.log(card);
-    console.log(deck);
+    // console.log(card);
+    // console.log(deck);
     return card;
 };
 
@@ -58,4 +66,39 @@ const cardValue = (card) => {
             : value * 1;
 };
 
-const value = cardValue(pickCard());
+// computer turn
+
+
+// const value = cardValue(pickCard());
+
+// Events
+btnRequestCard.addEventListener('click', () => {
+
+    createDeck();
+
+    const card = pickCard();
+    playerPoints += cardValue(card);
+    pointsContainer[0].innerText = playerPoints;
+
+    playerCards.append(createCardDOM(card));
+
+    playerLoose(playerPoints);
+});
+
+// Create card to the DOM
+
+const createCardDOM = (card) => {
+    const cardImg = document.createElement('img');
+    cardImg.classList.add('card');
+    cardImg.src = `./assets/cartas/${card}.png`;
+
+    return cardImg;
+};
+
+// Player loose
+
+const playerLoose = (points) => {
+    (points > 21) ? btnRequestCard.disabled = true
+        : (points === 21) ? btnRequestCard.disabled = true
+            : btnRequestCard.disabled = false;
+};
